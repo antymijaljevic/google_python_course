1f94ca66d3e9c523097b56e1b5f5ab71f8d3a064
antymijaljevic <antymijaljevic@gmail.com>
2020-11-29 20:00:41 +0000

    new

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/home/amijaljevic/anaconda3/bin/python3
+
+import re
+
+def rearrange_name(name):
+    result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+    return f"{result[2]}, {result[1]}"
+
+print(rearrange_name("Ante Mijaljevic"))
