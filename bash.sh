3298cc2437344a9db6cdd578dc20b04abe5afe8c
amijaljevic <antymijaljevic@gmail.com>
2020-12-01 19:45:19 +0000

    new

--- bash.sh
+++ bash.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
 
+#command and variables
+clear
+
+line="............................."
 echo "Starting at: $(date)"
 
-echo "UPTIME"
-uptime
-echo
+echo "UPTIME";uptime;echo $line
 
-echo "FREE"
-free
-echo
+echo "FREE";free;echo $line
 
-echo "WHO"
-who
-echo
+echo "WHO";who;echo $line
 
 echo "Finishing at: $(date)"
+
+
+#if else fi
+echo;echo;echo
+
+if grep "127.0.0.1" /etc/hosts; then
+    echo "Everything OK"
+else
+    echo "ERROR! 127.0.0.1 is not in /etc/hosts"
+fi
+
+#test command
+
+if test -n "$PATH"; then echo "your path is not empty";fi
+
+if [ -n "$PATH" ]; then echo "your path is not empty"; fi
+
+
+# while loop
+echo;echo
+
+n=1
+
+while [ $n -le 5 ]; do
+    echo "Iteration number $n"
+    ((n+=1))
+done
+
+# for loop
+
+for file in *.html; do
+    name=$(basename "$file" .html)
+    mv "$file" "$name.txt"
+done
--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import sys
+
+
+for line in sys.stdin:
+    print(line.upper())
+
+
+# print(sys.stdin)
+# print(sys.argv[0])
+# print(sys.argv[1])
+# print(sys.argv[2])
+
+
