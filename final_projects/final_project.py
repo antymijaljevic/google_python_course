66ba560403c7705a61eeb6a729ae652840d8ec3c
antymijaljevic <antymijaljevic@gmail.com>
2020-12-29 23:05:45 +0000

    new_upload

--- final_projects/final_project.py
+++ final_projects/final_project.py
@@ -35,3 +35,39 @@
 # else:
 #     print("STATUS {} SUCCESS!".format(r.status_code))
 
+
+#3 | run.py
+
+import os
+import requests
+
+description_dir = os.path.join(os.environ['HOME'] + '/Documents/coding_projects/google_python_course/final_projects/supplier-data/descriptions/')
+
+# list files from dir
+for txtFile in os.listdir(description_dir):
+    #print(txtFile)
+
+    # Pre-setup dict
+    descriptions_data = {'name': None, 'weight': None, 'description':None, 'image_name':None}
+    order = list(descriptions_data)
+
+    #open each file and create dict from info
+    with open(os.path.join(description_dir, txtFile)) as contentOfFile:
+        for index, line in enumerate(contentOfFile):
+            descriptions_data[order[index]] = line.strip()
+
+    #add .jpeg file to dict, set weight value to int for django model
+    descriptions_data['image_name'] = os.path.basename(txtFile).replace('.txt', '.jpeg')
+    descriptions_data['weight'] = int(descriptions_data['weight'].split()[0])
+
+    print(descriptions_data)
+    # #4 | post dict, clear dict
+    # print(feedbacks)
+    # response = requests.post('http://35.202.197.14/feedback', data=feedbacks)
+    # feedbacks = {}
+
+    # #5 | status code
+    # if not response.ok:
+    #     response.raise_for_status()
+    # else:
+    #     print("STATUS CODE 201 SUCCESS!")
